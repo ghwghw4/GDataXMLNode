0e9c851a8336eb1f3bac1b216328df66f1f33656
龚海伟 <dagehaoshuang@163.com>
2018-07-02 10:35:37 +0800

    Initial commit

---
+++ Example/GDataXMLNode/GXViewController.h
@@ -0,0 +1,13 @@
+//
+//  GXViewController.h
+//  GDataXMLNode
+//
+//  Created by ghwghw4 on 07/02/2018.
+//  Copyright (c) 2018 ghwghw4. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GXViewController : UIViewController
+
+@end
---
+++ Example/GDataXMLNode/GXViewController.m
@@ -0,0 +1,29 @@
+//
+//  GXViewController.m
+//  GDataXMLNode
+//
+//  Created by ghwghw4 on 07/02/2018.
+//  Copyright (c) 2018 ghwghw4. All rights reserved.
+//
+
+#import "GXViewController.h"
+
+@interface GXViewController ()
+
+@end
+
+@implementation GXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
